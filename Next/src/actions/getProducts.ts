00d996823058955d1c70ee78bc77ff68a0cce1d5
GuiVelMat <gvm0898@gmail.com>
2025-01-16 01:45:40 +0100

    Next. GetProducts envía correctamente la lista de productos

---
+++ Next/src/actions/getProducts.ts
@@ -0,0 +1,26 @@
+// pages/api/products.ts
+import { prisma } from '@/lib/prisma';
+import { ProductResponse } from '@/types/Product';
+
+export default async function getProducts (): Promise<ProductResponse> {
+    const data = await prisma.product.findMany({
+      include: {
+        categories: true,
+        types: true,
+        series: true,
+        collections: true,
+        ImagesProduct: true,
+      }
+    });
+
+    return {
+      products: data.map((product) => ({
+      ...product,
+      collection: product.collections,
+      categories: product.categories,
+      types: product.types,
+      series: product.series,
+      ImagesProduct: product.ImagesProduct,
+      })),
+    }
+}
