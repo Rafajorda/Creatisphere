5075bbb2b006d3dfa3127f0d91364b846361a56a
alexMartJu <alex.mart.juan@gmail.com>
2025-01-17 17:28:04 +0100

    Next. Page Home actualizada con ListCollections

--- Next/src/app/(browse)/(home)/page.tsx
+++ Next/src/app/(browse)/(home)/page.tsx
@@ -1,10 +1,12 @@
 import ListProducts from '@/components/shared/Lists/ListProducts';
+import ListCollections from '@/components/shared/Lists/ListCollections';
 import React from 'react';
 
 const Home = () => {
     return (
         <>
             <ListProducts />
+            <ListCollections />
         </>
     );
 };
