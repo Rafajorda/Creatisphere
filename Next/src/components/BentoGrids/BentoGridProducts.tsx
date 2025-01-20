c1d299d593a86fe33cd28c280d0f69a27cc38e27
Guido <gvm0898@gmail.com>
2025-01-20 20:56:08 +0100

    Next. Añadidos skeletons

--- Next/src/components/BentoGrids/BentoGridProducts.tsx
+++ Next/src/components/BentoGrids/BentoGridProducts.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { CardProduct } from '../shared/Cards/CardProduct';
 import { useFetchGalleryImages } from '@/hooks/useGalleryImages';
+import { SkeletonProduct } from '../shared/skeletons/skeletonProduct';
 
 const ImageGallery = async () => {
     const sortedImages = await useFetchGalleryImages();
@@ -8,14 +9,16 @@ const ImageGallery = async () => {
     return (
         <div className="bg-gray-900 py-1">
             <div
-                className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-1"
+                className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-1"
                 style={{
                     gridAutoRows: 'calc((100vw - 32px - 48px) / 5)',
                     gridAutoFlow: 'dense'
                 }}
             >
                 {sortedImages.map((product) => (
-                    <CardProduct key={product.id} product={product} />
+                    <Suspense fallback={<SkeletonProduct />} key={product.id}>
+                        <CardProduct product={product} key={product.id} />
+                    </Suspense>
                 ))}
             </div>
         </div>
