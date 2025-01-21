0f90d9d21b437d11fa0d5f4a0fca0cf8c91da89f
GuiVelMat <gvm0898@gmail.com>
2025-01-21 03:19:22 +0100

    Next. Seleccionado el color de fondo para el home

--- Next/src/components/BentoGrids/BentoGridSeries.tsx
+++ Next/src/components/BentoGrids/BentoGridSeries.tsx
@@ -8,9 +8,9 @@ const BentoGridSeries = async () => {
     const limitedSeriesList = seriesList.series.slice(0, 6);
 
     return (
-        <section className='bg-gradient-to-b from-black/70 to-transparent'>
+        <section className='bg-gradient-to-b from-transparent to-zinc-600'>
             <div className='container mx-auto text-shadow-lg'>
-                <h1 className='text-5xl italic font-bold text-white text-end'>Your favourite series</h1>
+                <h1 className='text-5xl italic font-bold text-white'>Your favourite series</h1>
                 <div className="grid max-h-screen h-auto p-4 md:grid-cols-2 lg:grid-cols-6 lg:grid-rows-3 gap-5 mt-5">
                     {limitedSeriesList.map((series, index) => (
                         <CardSeries key={series.id} series={series} ind={index} />
