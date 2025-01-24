ac4fb351770dae7134a1659434b36f1dd0b0ab0a
Guido <gvm0898@gmail.com>
2025-01-24 20:27:52 +0100

    Next. Mejorado el css de los filtros con accordeon de shadcn

--- Next/src/components/shared/filters.tsx
+++ Next/src/components/shared/filters.tsx
@@ -5,6 +5,7 @@ import { Filter } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useFilters } from "@/hooks/useFilters"
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger, SheetFooter, SheetOverlay, } from "@/components/ui/sheet"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import Search from "./search"
 
@@ -25,36 +26,48 @@ export function Filters() {
                     <Filter className="h-4 w-4" />
                 </Button>
             </SheetTrigger>
-            <SheetContent className="bg-zinc-700 w-full sm:max-w-md">
+            <SheetContent className="bg-zinc-800/80 w-full sm:max-w-md">
                 <ScrollArea className="h-[calc(100vh-4rem)] pr-4">
                     <SheetHeader>
                         <SheetTitle className="text-white">Filtros</SheetTitle>
-                        <SheetDescription className="text-gray-300">
-                            {/* Selecciona las categorías que deseas filtrar */}
-                        </SheetDescription>
+                        <SheetDescription></SheetDescription>
                     </SheetHeader>
                     <Search placeholder="Product's name" />
-                    <div className="grid gap-4 py-4">
-                        {Object.entries(filterList).map(([filter, indexes]) => (
-                            <div key={filter} className="space-y-2">
-                                <h3 className="font-bold text-white">{filter}</h3>
-                                <div className="flex flex-wrap gap-2">
-                                    {indexes.map((index) => (
-                                        <Button
-                                            key={index}
-                                            variant={isActive(filter, index) ? "default" : "outline"}
-                                            size="sm"
-                                            aria-label={filter}
-                                            className="font-bold"
-                                            onClick={() => toggleFilterValue(filter, index)}
-                                        >
-                                            {index}
-                                        </Button>
-                                    ))}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                    <Accordion type="multiple" className="w-full">
+                        <div className="grid gap-4 py-4">
+                            {Object.entries(filterList).map(([filter, indexes]) => (
+                                <AccordionItem key={filter} value={filter} className="space-y-2 text-white">
+                                    <AccordionTrigger>{filter}</AccordionTrigger>
+                                    <AccordionContent>
+                                        <div key={filter} className="space-y-2">
+                                            <div className="flex flex-wrap gap-2">
+                                                {indexes.map((index) => (
+                                                    <Button
+                                                        key={index}
+                                                        variant={isActive(filter, index) ? "outline" : "outline"}
+                                                        size="sm"
+                                                        aria-label={filter}
+                                                        className={`font-bold ${isActive(filter, index) ? "text-zinc-700 bg-white" : "text-white bg-zinc-700"
+                                                            }`}
+                                                        onClick={() => toggleFilterValue(filter, index)}
+                                                    >
+                                                        {index}
+                                                    </Button>
+                                                ))}
+
+                                            </div>
+                                        </div>
+                                    </AccordionContent>
+                                </AccordionItem>
+
+                            ))}
+                            <Button
+                                // variant="destructive"
+                                className="text-xl bg-gold text-black font-bold hover:bg-light-gold mt-3"
+                                onClick={() => toggleFilterValue('', '')}
+                            >Reset Filters</Button>
+                        </div>
+                    </Accordion>
                 </ScrollArea>
                 <SheetFooter />
             </SheetContent>
