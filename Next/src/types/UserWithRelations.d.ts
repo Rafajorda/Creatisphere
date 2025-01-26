9c95cd76b27a9f657beeea2f3dbae73362085981
alexMartJu <alex.mart.juan@gmail.com>
2025-01-26 17:50:05 +0100

    Next. Implementar mapper de usuario con relaciones para perfil

---
+++ Next/src/types/UserWithRelations.d.ts
@@ -0,0 +1,41 @@
+import { User, Profile, Product, Favorite, Cart, Order, Returns, Notification } from '@prisma/client';
+
+
+type UserWithRelations = User & {
+  profile: Profile | null;
+  products: (Product & {
+    categories: Category[];
+    types: Type[];
+    series: Series;
+    collections: Collection;
+    ImagesProduct: ImagesProduct[];
+  })[];
+  favorites: (Favorite & {
+    favoriting: Product & {
+      categories: Category[];
+      types: Type[];
+      series: Series;
+      collections: Collection;
+      ImagesProduct: ImagesProduct[];
+      artist: User & { profile: Profile | null };
+    };
+  })[];
+  following: { followerId: number; followingId: number; following: User & { profile: Profile | null } }[]; // Aquí cambiamos el tipo para incluir al usuario seguido
+  followers: { followerId: number; followingId: number;follower: User & { profile: Profile | null } }[];
+  notifications: Notification[];
+  Cart: (Cart & {
+    cartLines: (CartLine & {
+      product: Product;
+    })[];
+  })[];
+  Order: (Order & {
+    orderLines: (OrderLine & {
+      product: Product;
+    })[];
+  })[];
+  returns: (Returns & {
+    returnsLine: (ReturnsLine & {
+      product: Product;
+    })[];
+  })[];
+};
