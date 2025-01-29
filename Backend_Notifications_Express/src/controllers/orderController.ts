22a4aee7bf11acfd7e1d8f8390364fc8aa64206f
Rafajorda <rafajorgis@gmail.com>
2025-01-29 17:13:55 +0100

    Backend  Express Notificaciones notificacion para mandar email de la factura

---
+++ Backend_Notifications_Express/src/controllers/orderController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { AppError } from '../utils/AppError';
+import prismaGetOrder from '../repo/Order/PrismaGetOrder';
+import CreateOrder from '../services/Order/CreateOrder';
+import { Order, OrderLine } from '@prisma/client';
+
+export const orders = async (req: Request, res: Response): Promise<void> => {
+    try {
+       const { orderId } = req.body;
+
+       if (!orderId) {
+           res.status(400).json({ message: 'Required data missing: orderId' });
+           return;
+       }
+       const order = await prismaGetOrder(orderId);
+
+        if (!order) {
+            res.status(404).json({ message: 'Order not found' });
+            return;
+        }
+
+        const newNotification = await CreateOrder(order);
+
+        res.status(201).json(newNotification);
+
+    } catch (err) {
+        if (err instanceof AppError) {
+            res.status(err.statusCode).json({ message: err.message });
+        } else {
+            console.error('Unexpected error in Orders:', err);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    }
+}
--- Backend_Notifications_Express/src/routes/notificationsRoutes.ts
+++ Backend_Notifications_Express/src/routes/notificationsRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
  import { Likes } from '../controllers/likesController';
  import { Follow } from '../controllers/followController';
+ import { orders } from '../controllers/orderController';
 
 
 const router = Router();
 
  router.post('/Likes', Likes);
  router.post('/Follow', Follow);
+ router.post('/Orders', orders);
 
 export default router;
