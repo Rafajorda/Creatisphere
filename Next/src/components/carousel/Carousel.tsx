48a0d271ab97095d3a5a4f392c107c4e6854de44
GuiVelMat <gvm0898@gmail.com>
2025-01-30 21:28:53 +0100

    Next. Carousel redirige correctamente

--- Next/src/components/carousel/Carousel.tsx
+++ Next/src/components/carousel/Carousel.tsx
@@ -2,9 +2,10 @@ import React from "react"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import getCarousel from "@/actions/getCarousel"
 import Image from "next/image"
+import Link from "next/link"
 
 const CarouselHome = async () => {
-    const carouselSlides = await getCarousel()
+    const carouselSlides = await getCarousel();
 
     return (
         <div className="relative">
@@ -12,18 +13,20 @@ const CarouselHome = async () => {
                 <CarouselContent>
                     {carouselSlides.carousels.map((slide) => (
                         <CarouselItem key={slide.id} className="relative group">
-                            <Image src={`/assets/carousel/${slide.image}`} alt={slide.title} width={1900} height={700} />
-                            <div className="absolute bottom-0 left-0 right-0 h-2/3">
-                                <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-                                <div className="absolute bottom-10 left-40 right-0 p-6 text-white font-serif italic text-shadow-xl select-none">
-                                    <h3 className="text-4xl md:text-8xl font-bold opacity-50 group-hover:opacity-100 transition-opacity duration-300">
-                                        {slide.title}
-                                    </h3>
-                                    <p className="ml-20 md:text-4xl mt-2 opacity-50 group-hover:opacity-100 transition-opacity duration-300">
-                                        {slide.description}
-                                    </p>
+                            <Link href={slide.href}>
+                                <Image src={`/assets/carousel/${slide.image}`} alt={slide.title} width={1900} height={700} />
+                                <div className="absolute bottom-0 left-0 right-0 h-2/3">
+                                    <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                                    <div className="absolute bottom-10 left-40 right-0 p-6 text-white font-serif italic text-shadow-xl select-none">
+                                        <h3 className="text-4xl md:text-8xl font-bold opacity-50 group-hover:opacity-100 transition-opacity duration-300">
+                                            {slide.title}
+                                        </h3>
+                                        <p className="ml-20 md:text-4xl mt-2 opacity-50 group-hover:opacity-100 transition-opacity duration-300">
+                                            {slide.description}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
                         </CarouselItem>
                     ))}
                 </CarouselContent>
