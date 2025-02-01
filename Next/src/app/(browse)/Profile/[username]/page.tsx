87590f243c7158953068ddac5b9228a628a41425
GuiVelMat <gvm0898@gmail.com>
2025-02-01 17:23:02 +0100

    Next. Adaptada la page de profile con el profileTabs

--- Next/src/app/(browse)/Profile/[username]/page.tsx
+++ Next/src/app/(browse)/Profile/[username]/page.tsx
@@ -2,11 +2,8 @@ import { Metadata } from 'next'
 import React from 'react'
 import { useGetProfile } from '@/hooks/useAuth'
 import { ProfileInfo } from '@/components/Profile/ProfileInfo'
-import { ProfileFollow } from '@/components/Profile/ProfileFollow'
-import { ProfileFavorites } from '@/components/Profile/ProfileFavorites'
-import { ProfileProducts } from '@/components/Profile/ProfileProducts'
-import { EditProfileButton } from '@/components/shared/buttons/EditProfileButton'
-import getCurrentUser from '@/actions/getCurrentUser'
+import { ProfileTabs } from '@/components/Profile/ProfileTabs'
+import { redirect } from 'next/navigation'
 
 interface ProfilePageProps {
   params: { username: string }
@@ -15,35 +12,24 @@ interface ProfilePageProps {
 export async function generateMetadata({
   params,
 }: ProfilePageProps): Promise<Metadata> {
-
   const { username } = await params;
   const profile = await useGetProfile(username)
   return profile ? { title: profile.username } : {}
 }
 
+
 const ProfilePage = async ({ params }: ProfilePageProps) => {
-  const currentUser = await getCurrentUser()
   const { username } = await params;
   const profile = await useGetProfile(username)
 
-  const isCurrentUser = currentUser?.profile?.username === profile?.username
-
   if (!profile) {
-    return null // Si no se encuentra el perfil, redirige
+    redirect('/404')
   }
 
   return (
     <div className="profile-page text-white text-xl container mx-auto">
       <ProfileInfo profile={profile} />
-
-      {isCurrentUser && (
-        <EditProfileButton />
-      )}
-
-      <ProfileProducts profile={profile} />
-      <ProfileFavorites profile={profile} />
-      <ProfileFollow profile={profile} isFollower={false} /> {/* Following */}
-      <ProfileFollow profile={profile} isFollower={true} /> {/* Followers */}
+      <ProfileTabs profile={profile} />
     </div>
   )
 }
