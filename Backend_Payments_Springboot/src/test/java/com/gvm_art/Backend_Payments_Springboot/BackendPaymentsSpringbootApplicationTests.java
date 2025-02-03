ef55ff4819c3c586ea1fa733e30ee9f0a474e0d9
alexMartJu <alex.mart.juan@gmail.com>
2025-02-03 11:10:26 +0100

    Backend_Payments_Springboot. Estructura inicial de Spring Boot

---
+++ Backend_Payments_Springboot/src/test/java/com/gvm_art/Backend_Payments_Springboot/BackendPaymentsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvm_art.Backend_Payments_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPaymentsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
