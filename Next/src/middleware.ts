873a0373b271b592b42f0550a69b9b6556b3f9e9
GuiVelMat <gvm0898@gmail.com>
2025-02-05 00:30:55 +0100

    Next. Cambiada la lógica de middleware a Chain.ts

--- Next/src/middleware.ts
+++ Next/src/middleware.ts
@@ -1,32 +1,5 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
-// import getCurrentUser from "./actions/getCurrentUser"
+import { chain } from "./middlewares/Chain"
+import { AuthGuard } from "./middlewares/AuthGuard"
+import { PremiumGuard } from "./middlewares/PremiumGuard"
 
-const 
-
-export function middleware(request: NextRequest) {
-    const isLogged = request.cookies.get("next-auth.session-token")?.value
-    // const currentUser = await getCurrentUser();
-
-    if (isLogged && (request.nextUrl.pathname === "/Login" || request.nextUrl.pathname === "/Register")) {
-        return NextResponse.redirect(new URL("/", request.url))
-    } // Si estás logeado, no puedes entrar a Login o Register
-
-    if (!isLogged && request.nextUrl.pathname === '/settings') {
-        return NextResponse.redirect(new URL("/Login", request.url))
-    } // Si no estás logeado, no puedes entrar a settings
-
-    if (!isLogged && request.nextUrl.pathname === '/Premium') {
-        return NextResponse.redirect(new URL('/GetPremium', request.url))
-    } // Si no estás logeado, no puedes entrar a premium
-
-    // if (currentUser?.role === 'USER' && request.nextUrl.pathname === '/Premium') {
-    //     return NextResponse.redirect(new URL('/GetPremium', request.url))
-    // } // Si eres un usuario normal, no puedes entrar a premium
-
-    return NextResponse.next()
-}
-
-export const config = {
-    matcher: ["/Login", "/Register", "/settings", "/Premium", "/GetPremium"],
-}
+export default chain([AuthGuard, PremiumGuard])
