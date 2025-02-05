dc11d71c3c3f22d022ce8d5e98a2f5be2af8d39a
Guido <gvm0898@gmail.com>
2025-02-05 20:41:02 +0100

    Next. Page premium

--- Next/src/app/(browse)/Premium/page.tsx
+++ Next/src/app/(browse)/Premium/page.tsx
@@ -1,11 +1,22 @@
+import getCurrentUser from '@/actions/getCurrentUser';
+import getPremiumPosts from '@/actions/getPremiumPosts';
+import { CardPremiumPost } from '@/components/shared/Cards/CardPremiumPost';
 import React from 'react';
 
-const Premium = () => {
+const Premium = async () => {
+    const currentUser = await getCurrentUser();
+    const content = await getPremiumPosts();
+    const isPremium = currentUser?.role === 'PREMIUM';
+
     return (
-        <>
-            <h1>Premium content</h1>
-        </>
+        <div className="container mx-auto p-4 my-10">
+            <div className="space-y-8">
+                {content.premiumPosts.map((item) => (
+                    <CardPremiumPost key={item.id} item={item} isPremium={isPremium} />
+                ))}
+            </div>
+        </div>
     );
 };
 
-export default Premium
+export default Premium;
