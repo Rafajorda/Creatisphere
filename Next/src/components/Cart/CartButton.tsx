62aad3930a16bfacf2b131f7d57f0a5190e21132
GuiVelMat <gvm0898@gmail.com>
2025-02-08 22:48:45 +0100

    Next. Dividido cartList y cartcard

--- Next/src/components/Cart/CartButton.tsx
+++ Next/src/components/Cart/CartButton.tsx
@@ -11,7 +11,7 @@ export const CartButton = async () => {
 
     return (
         <Link href="/Cart">
-            <button className="fixed bottom-10 left-10 bg-zinc-800 text-white p-3 rounded-full shadow-lg hover:bg-zinc-700 transition-colors">
+            <button className="fixed bottom-10 left-10 bg-zinc-800 text-white p-3 rounded-full shadow-lg hover:bg-zinc-700 transition-colors border-filter">
                 <ShoppingCart className="w-6 h-6" />
                 {cartLinesLength > 0 && (
                     <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
---
+++ Next/src/components/shared/Cards/CardCart.tsx
@@ -0,0 +1,37 @@
+import type React from "react"
+import Image from "next/image"
+import Link from "next/link";
+
+interface CartItemProps {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    item: any
+}
+
+export const CardCart = ({ item }: CartItemProps) => {
+    console.log(item);
+    const size = item.productPrice.product.ImagesProduct[0].size;
+
+    return (
+        <div className="grid grid-cols-3 gap-4 py-4 border-b border-gray-500">
+            <div className="col-span-2 flex justify-center items-center">
+                <div className={`relative ${size === "tall" ? "h-96 w-56" : "h-56 w-96"}`}>
+                    <Link href={`/Details/${item.productPrice.product.slug}`}>
+                        <Image
+                            src={`/assets/products/${item.productPrice.product.ImagesProduct[0].src}`}
+                            alt={item.productPrice.product.name}
+                            layout="fill"
+                            objectFit="cover"
+                            className="rounded-md"
+                        />
+                    </Link>
+                </div>
+            </div>
+            <div className="flex flex-col justify-center text-end mr-3">
+                <h2 className="text-xl text-gray-300">{item.types.name}</h2>
+                <h2 className="text-2xl font-semibold text-white mb-5">{item.productPrice.product.name}</h2>
+                <p className="text-xl text-gray-400">{item.productPrice.price.toFixed(2)}€</p>
+            </div>
+        </div>
+    )
+}
+
