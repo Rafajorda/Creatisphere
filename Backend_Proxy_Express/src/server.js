a2970df4b748e3fba384a276ab956e3befd524d1
alexMartJu <alex.mart.juan@gmail.com>
2025-02-12 17:34:06 +0100

    Proxy_Express. Reimplementación servidor proxy con http-proxy y añade configuración dinámica

--- Backend_Proxy_Express/src/server.js
+++ Backend_Proxy_Express/src/server.js
@@ -1,17 +1,64 @@
-const express = require('express');
-const cors = require('cors');
-const { configureProxies } = require('./middlewares/proxyConfig');
-const { PORT } = require('./config');
+const http = require('http');
+const httpProxy = require('http-proxy');
+const { 
+    URL_PROXY, 
+    PORT_PROXY, 
+    URL_SPRING, 
+    PORT_SPRING,
+    URL_EXPRESS, 
+    PORT_EXPRESS 
+} = require('./config');
 
-const app = express();
+const proxy = httpProxy.createProxyServer({
+    changeOrigin: true,
+    secure: false
+});
 
-// Configurar CORS
-app.use(cors());
+const routes = {
+    '/api/payment': {
+        target: URL_SPRING,
+        port: PORT_SPRING
+    },
+    '/api/notifications': {
+        target: URL_EXPRESS,
+        port: PORT_EXPRESS
+    }
+};
 
-// Configurar proxies
-configureProxies(app);
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const route = Object.keys(routes).find(path => req.url.startsWith(path));
+    const targetConfig = routes[route];
+    
+    proxyReq.setHeader('origin', 'http://localhost:4000');
+    proxyReq.setHeader('host', `localhost:${targetConfig.port}`);
+});
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor Proxy escuchando en el puerto ${PORT}`);
+proxy.on('proxyRes', (proxyRes, req, res) => {
+    delete proxyRes.headers['access-control-allow-origin'];
+    proxyRes.headers['access-control-allow-origin'] = 'http://localhost:3000';
+});
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
+    const route = Object.keys(routes).find(path => req.url.startsWith(path));
+    if (route) {
+        console.log(`Redirigiendo a ${routes[route].target}`);
+        proxy.web(req, res, { target: routes[route].target });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(PORT_PROXY, () => {
+    console.log('Proxy server running at ' + URL_PROXY);
 });
