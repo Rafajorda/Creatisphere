8c143a1c915043d03ef2e444d90d3249dad270bb
Rafajorda <rafajorgis@gmail.com>
2025-03-02 18:44:38 +0100

    delete series

--- Next/src/app/api/collections/route.ts
+++
@@ -1,7 +0,0 @@
-import getCollections from '@/actions/getCollections';
-import { NextResponse } from 'next/server';
-
-export const GET = async () => {
-    const collections = await getCollections();
-    return NextResponse.json(collections);
-}
--- Next/src/types/Series.d.ts
+++
@@ -1,9 +0,0 @@
-import { Series, Product } from "@prisma/client";
-
-export interface SeriesItem extends Omit<Series, 'updatedAt'> {
-    products: Product[];
-}
-
-export interface SeriesResponse {
-    series: SeriesItem[];
-}
