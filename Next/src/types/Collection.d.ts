7bb246f39d994dc6db0ff87fddbcbf6398c0a7f5
Rafajorda <rafajorgis@gmail.com>
2025-03-02 18:44:03 +0100

    delete collection

--- Next/src/types/Collection.d.ts
+++
@@ -1,9 +0,0 @@
-import { Collection, Product } from "@prisma/client";
-
-export interface CollectionItem extends Omit<Collection, 'updatedAt'> {
-    products: Product[];
-}
-
-export interface CollectionResponse {
-    collections: CollectionItem[];
-}
